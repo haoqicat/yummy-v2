7dba437145cdbcd6d1a2f29face9d2d432c01a80
Peter Wang <happypeter1983@gmail.com>
2018-04-05 09:48:05 +0800

    2-svg

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import HomeContainer from '../containers/HomeContainer'
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <HomeContainer />
+      </div>
+    )
+  }
+}
+
+export default Main
