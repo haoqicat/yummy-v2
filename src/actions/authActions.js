2908a110a9d553f407e3411103a7c7a887758d82
Peter Wang <happypeter1983@gmail.com>
2018-04-08 19:45:24 +0800

    8-history

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import { SIGNUP_URL, LOGIN_URL } from '../constants/ApiConstants'
+import { history } from '../utils/routerUtils'
 
 export const signup = data => dispatch => {
   axios
     .post(SIGNUP_URL, data)
     .then(res => {
       console.log('res', res.data)
+      history.push('/dashboard')
     })
     .catch(err => {
       if (err.response) {
@@ -21,6 +23,7 @@ export const login = data => {
       .post(LOGIN_URL, data)
       .then(res => {
         console.log('res', res.data)
+        history.push('/dashboard')
       })
       .catch(err => {
         if (err.response) {
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import HomeContainer from '../containers/HomeContainer'
 import LayoutContainer from '../containers/LayoutContainer'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Router } from 'react-router'
+import { history } from '../utils/routerUtils'
+import { Switch, Route } from 'react-router-dom'
 
 class Main extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={HomeContainer} />
           <Route component={LayoutContainer} />
